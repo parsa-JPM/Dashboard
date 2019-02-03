6d7f0440569d5a188101112a4957109d8320ba01
parsa mihandoost <mihandoost.p@gmail.com>
2019-02-03 19:55:36 +0330

    add good join example

--- src/main/java/ir/codefather/dashboard/modules/post/repositories/PostTypeRepo.java
+++ src/main/java/ir/codefather/dashboard/modules/post/repositories/PostTypeRepo.java
@@ -1,7 +1,17 @@
 package ir.codefather.dashboard.modules.post.repositories;
 
+import ir.codefather.dashboard.modules.post.model.Post;
 import ir.codefather.dashboard.modules.post.model.PostType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostTypeRepo extends JpaRepository<PostType, Integer> {
+
+    // join example
+    @Query("select type from PostType type inner join type.posts p where p.locale = :l")
+    List<PostType> getCustom(@Param("l") String locale);
+
 }
--- src/main/java/ir/codefather/dashboard/modules/test/controller/TestController.java
+++ src/main/java/ir/codefather/dashboard/modules/test/controller/TestController.java
@@ -1,35 +1,27 @@
 package ir.codefather.dashboard.modules.test.controller;
 
-import ir.codefather.dashboard.modules.test.model.Cart;
-import ir.codefather.dashboard.modules.test.model.Items;
-import ir.codefather.dashboard.modules.test.repositories.CartRepo;
-import ir.codefather.dashboard.modules.test.repositories.ItemsRepo;
+import ir.codefather.dashboard.modules.post.model.Post;
+import ir.codefather.dashboard.modules.post.model.PostType;
+import ir.codefather.dashboard.modules.post.repositories.PostRepo;
+import ir.codefather.dashboard.modules.post.repositories.PostTypeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TestController {
 
     @Autowired
-    private CartRepo cartRepo;
+    private PostRepo postRepo;
     @Autowired
-    private ItemsRepo itemsRepo;
+    private PostTypeRepo postTypeRepo;
 
     @RequestMapping("/test")
     @ResponseBody
-    public String test() {
-        Cart cart = new Cart();
-        Items items=new Items();
-
-        cart.setItems(Arrays.asList(items));
-
-        itemsRepo.save(items);
-        cartRepo.save(cart);
-
-        return "done";
+    public List<PostType> test(String locale) {
+        return postTypeRepo.getCustom(locale);
     }
 }
